89a0a64c13ada48424d2c0918b536bfb315a2931
Michael <mbahl1670@gmail.com>
2021-12-21 22:06:57 -0600

    password module exported

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+const password = require("./password");
 
 const db = mysql.createConnection({
   host: 'localhost',
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: '',
+  password: password,
   database: 'election'
 });
 
