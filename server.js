df12ad871f9c25f938502337183b6dfe08a3d11f
Michael <mbahl1670@gmail.com>
2021-12-21 21:33:51 -0600

    12.4.4

--- server.js
+++ server.js
@@ -12,64 +12,20 @@ app.use(express.json());
 app.use('/api', apiRoutes);
 
 
-app.get("/api/parties", (req,res) => {
-    const sql = `SELECT * FROM parties`;
-
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error:err.message });
-            return;
-        }
-        res.json({
-            message: "success",
-            data: rows
-        });
-    });
-});
-
-app.get("/api/party/:id", (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-
-    db.query(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: "success",
-            data: row
-        });
-    });
-});
-
-app.delete("/api/party/:id", (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: res.message });
-            return;
-        } else if (!result.affectedRows) {
-            res.json({
-                message: "Party not found"
-            });
-        } else {
-            res.json({
-                message: "deleted",
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
-
 // Defalut response for any other requests (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log("Database connected.");
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
