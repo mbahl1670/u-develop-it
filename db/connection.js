58647cf6035dc5fc9397e0059b93f8ba4bbb0a9e
Michael <mbahl1670@gmail.com>
2021-12-21 21:47:40 -0600

    commit

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,12 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
-
-// Connect to database
-const db = mysql.createConnection(
-    {
-        host: "localhost",
-        // Your MySQL username,
-        user: "root",
-        // Your MySQL password
-        password: "#FUapple2",
-        database: "election"
-    },
-    console.log("Connected to the election database.")
-);
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: '',
+  database: 'election'
+});
 
 module.exports = db;
