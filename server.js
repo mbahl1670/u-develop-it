7779a86a99bf722c17d696469d6366fefc8ca8d0
Michael <mbahl1670@gmail.com>
2021-12-21 21:57:44 -0600

    password hidden

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ db.connect(err => {
     if (err) throw err;
     console.log("Database connected.");
     app.listen(PORT, () => {
+        console.log("password hidden");
         console.log(`Server running on port ${PORT}`);
     });
 });
