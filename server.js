2dab125cc53aa194ab9abf1f60fe911ee312500a
Michael <mbahl1670@gmail.com>
2021-12-21 18:11:04 -0600

    12.3.5

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name AS party_name 
+                 FROM candidates
+                 LEFT JOIN parties ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if(err) {
@@ -40,9 +42,13 @@ app.get("/api/candidates", (req, res) => {
 
 // GET a single candidate
 app.get(`/api/candidate/:id`, (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name AS party_name 
+                 FROM candidates 
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id 
+                 WHERE candidates.id = ?`;
     const params = [req.params.id];
-    console.log("hi")
+    
     db.query(sql, params, (err, row) => {
         if (err) {
             res.status(400).json({ error: err.message });
@@ -54,6 +60,23 @@ app.get(`/api/candidate/:id`, (req, res) => {
         });
     });
 });
+
+// // GET a single candidate
+// app.get(`/api/candidate/:id`, (req, res) => {
+//     const sql = `SELECT * FROM candidates WHERE id = ?`;
+//     const params = [req.params.id];
+    
+//     db.query(sql, params, (err, row) => {
+//         if (err) {
+//             res.status(400).json({ error: err.message });
+//             return;
+//         } 
+//         res.json({
+//             message: "success",
+//             data: row
+//         });
+//     });
+// });
 
 // Delete a candidate
 app.delete("/api/candidate/:id", (req , res) => {
